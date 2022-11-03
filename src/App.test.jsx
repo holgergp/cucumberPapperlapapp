d3042881458b47a6cfd00b5ca68d99ad87c62d90
Holger Grosse-Plankermann <holgergp@gmail.com>
2022-11-03 17:06:43 +0100

    Clunky test file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Die Anwendung", () => {
+  const mockStore = configureStore();
+  const store = mockStore({
+    bohnen: [
+      {
+        id: "1",
+        art: "Äthiopien",
+        vkp: 12.0,
+        marge: 20.0,
+        ekp: 10.0,
+        rabatt: 0.0,
+        vkpRabatt: 12.0,
+      },
+    ],
+  });
+  it("Soll folgende Bohnendaten zeigen", () => {
+    render(<App store={store} />);
+    expect(document.querySelector("#art").value).toEqual("Äthiopien");
+    expect(document.querySelector("#ekp").value).toEqual("10");
+    expect(document.querySelector("#vkp").value).toEqual("12");
+    expect(document.querySelector("#marge").value).toEqual("20");
+  });
 });
+
